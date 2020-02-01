207d00646997398f48195db08a28a78a05a05fcd
Marc Jakobi <marc.jakobi@velasolaris.com>
2020-02-01 23:35:39 +0100

    Modify indeterminate progress bar based on metal UI.

--- src/main/java/mrcjkb/material/swing/ui/MaterialProgressBarUI.java
+++ src/main/java/mrcjkb/material/swing/ui/MaterialProgressBarUI.java
@@ -1,14 +1,12 @@
 package mrcjkb.material.swing.ui;
 
-import java.awt.*;
-import java.awt.geom.Rectangle2D;
-import java.awt.geom.RoundRectangle2D;
+import mrcjkb.material.swing.materialui.util.MaterialDrawingUtils;
 
 import javax.swing.*;
 import javax.swing.plaf.ComponentUI;
 import javax.swing.plaf.basic.BasicProgressBarUI;
-
-import mrcjkb.material.swing.materialui.util.MaterialDrawingUtils;
+import java.awt.*;
+import java.awt.geom.RoundRectangle2D;
 
 /**
  * Code based on the ProggressBarUIs from the following two projects:
@@ -98,56 +96,65 @@ public class MaterialProgressBarUI extends BasicProgressBarUI {
 
 	@Override
 	protected void paintIndeterminate(Graphics g, JComponent c) {
-		Graphics2D g2 = (Graphics2D) MaterialDrawingUtils.getAliasedGraphics(g).create();
-		g2.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_NORMALIZE);
-		try {
-			Rectangle r = new Rectangle(progressBar.getSize());
-			if (c.isOpaque()) {
-				g2.setColor(c.getParent().getBackground());
-				g2.fill(r);
-			}
 
-			removeInsets(r);
-			Insets i = progressBar.getInsets();
-			int amountFull = getAmountFull(i, r.width, r.height);
+		if (!(g instanceof Graphics2D)) {
+			return;
+		}
 
+		Insets b = progressBar.getInsets(); // area for border
+		int barRectWidth = progressBar.getWidth() - (b.right + b.left);
+		int barRectHeight = progressBar.getHeight() - (b.top + b.bottom);
 
-			int orientation = progressBar.getOrientation();
+		if (barRectWidth <= 0 || barRectHeight <= 0) {
+			return;
+		}
 
-			Color startColor = progressBar.getBackground();
-			Color endColor = progressBar.getForeground();
+		Graphics2D g2 = (Graphics2D) g;
 
+		Rectangle r = new Rectangle(progressBar.getSize());
+		removeInsets(r);
+		Insets i = progressBar.getInsets();
+		int amountFull = getAmountFull(i, r.width, r.height);
+
+		// Paint the bouncing box.
+		boxRect = getBox(boxRect);
+		removeInsets(boxRect);
+		Shape fullShape, coloredShape = null;
+		int orientation = progressBar.getOrientation();
+		if (orientation == SwingConstants.HORIZONTAL) {
 			int pHeight = progressBar.getPreferredSize().height;
-			int pWidth = progressBar.getPreferredSize().width;
-
 			int yOffset = r.y + (r.height - pHeight) / 2;
+			int radius = pHeight >>> 1;
+
+			fullShape = new RoundRectangle2D.Float(r.x, yOffset, r.width, pHeight, radius, radius);
+			if (boxRect != null) {
+				radius = boxRect.height >>> 1;
+				coloredShape = new RoundRectangle2D.Float(boxRect.x, yOffset, boxRect.width, pHeight, radius, radius);
+			}
+		} else {
+			int pWidth = progressBar.getPreferredSize().width;
 			int xOffset = r.x + (r.width - pWidth) / 2;
+			int radius = pWidth >>> 1;
+			fullShape = new RoundRectangle2D.Float(xOffset, r.y, pWidth, r.height, radius, radius);
+			if (boxRect != null) {
+				radius = boxRect.width >>> 1;
+				coloredShape = new RoundRectangle2D.Float(xOffset, boxRect.y, pWidth, boxRect.height, radius, radius);
+			}
+		}
+		g2.setColor(progressBar.getBackground());
+		g2.fill(fullShape);
+		if (boxRect != null) {
+			g2.setColor(progressBar.getForeground());
+			g2.fill(coloredShape);
+		}
 
-			Shape shape;
-			int step = 6;
-			if (orientation == SwingConstants.HORIZONTAL) {
-				shape = new RoundRectangle2D.Float(r.x, yOffset, r.width, pHeight, pHeight, pHeight);
-				yOffset = r.y + pHeight / 2;
-				g2.setPaint(new GradientPaint(r.x + getAnimationIndex() * step * 2, yOffset, startColor,
-						r.x + getFrameCount() * step + getAnimationIndex() * step * 2, yOffset, endColor, true));
+		// Deal with possible text painting
+		if (progressBar.isStringPainted()) {
+			if (progressBar.getOrientation() == SwingConstants.HORIZONTAL) {
+				paintString(g, i.left, i.top, r.width, r.height, amountFull, i);
 			} else {
-				shape = new RoundRectangle2D.Float(xOffset, r.y, pWidth, r.height, pWidth, pWidth);
-				xOffset = r.x + pWidth / 2;
-				g2.setPaint(new GradientPaint(xOffset, r.y + getAnimationIndex() * step * 2, startColor,
-						xOffset, r.y + getFrameCount() * step + getAnimationIndex() * step * 2, endColor, true));
+				paintString(g, i.left, i.top, r.width, r.height, amountFull, i);
 			}
-			g2.fill(shape);
-
-			// Paint text
-			if (progressBar.isStringPainted()) {
-				if (progressBar.getOrientation() == SwingConstants.HORIZONTAL) {
-					paintString(g, i.left, i.top, r.width, r.height, amountFull, i);
-				} else {
-					paintString(g, i.left, i.top, r.width, r.height, amountFull, i);
-				}
-			}
-		} finally {
-			g2.dispose();
 		}
 	}
 
