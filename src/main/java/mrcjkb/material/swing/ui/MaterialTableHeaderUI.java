70c9fb3a519bef38a1e72cc711291440f760c598
Marc Jakobi <marc.jakobi@velasolaris.com>
2020-10-29 09:08:03 +0100

    Revert "Replace TableCellRenderer and TableHeaderCellRenderer implementations with delegation."
    This reverts commit 19cd7d4fd6a025e9143875870558c370ff88c262.

--- src/main/java/mrcjkb/material/swing/ui/MaterialTableHeaderUI.java
+++ src/main/java/mrcjkb/material/swing/ui/MaterialTableHeaderUI.java
@@ -7,9 +7,7 @@ import javax.swing.plaf.ComponentUI;
 import javax.swing.plaf.basic.BasicTableHeaderUI;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.JTableHeader;
-import javax.swing.table.TableCellRenderer;
 import java.awt.*;
-import java.io.Serializable;
 
 public class MaterialTableHeaderUI extends BasicTableHeaderUI {
 
@@ -22,7 +20,7 @@ public class MaterialTableHeaderUI extends BasicTableHeaderUI {
 		super.installUI(c);
 
 		JTableHeader header = (JTableHeader) c;
-		header.setDefaultRenderer(new MaterialTableHeaderCellRendererDecorator(header.getDefaultRenderer()));
+		header.setDefaultRenderer(new MaterialTableHeaderCellRenderer());
 	}
 
 	@Override
@@ -34,7 +32,6 @@ public class MaterialTableHeaderUI extends BasicTableHeaderUI {
 		header.setForeground(null);
 		header.setFont(null);
 		header.setBorder(null);
-		header.setOpaque(false);
 
 		super.uninstallUI(c);
 	}
@@ -44,22 +41,16 @@ public class MaterialTableHeaderUI extends BasicTableHeaderUI {
 		super.paint(MaterialDrawingUtils.getAliasedGraphics(g), c);
 	}
 	
-	public static class MaterialTableHeaderCellRendererDecorator implements TableCellRenderer, Serializable {
-
-		private final TableCellRenderer tableCellRenderer;
+	public static class MaterialTableHeaderCellRenderer extends DefaultTableCellRenderer {
 
 		private static final long serialVersionUID = 1L;
 
-		public MaterialTableHeaderCellRendererDecorator(TableCellRenderer tableCellRenderer) {
-			this.tableCellRenderer = tableCellRenderer;
-		}
-
 		@Override
 		public Component getTableCellRendererComponent (JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
-			JComponent component = (JComponent) tableCellRenderer.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
-			if (tableCellRenderer instanceof DefaultTableCellRenderer) {
-				((DefaultTableCellRenderer) tableCellRenderer).setVerticalAlignment(SwingConstants.CENTER);
-			}
+			JComponent component = (JComponent) super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+
+			this.setVerticalAlignment(SwingConstants.CENTER);
+
 			return component;
 		}
 	}
